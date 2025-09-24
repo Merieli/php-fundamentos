86210925666be9af2b41042a4cd6e9c3906ec895
Merieli Manzano <merieli.amanzano@gmail.com>
2025-09-23 22:03:12 -0300

    decorator pattern

---
+++ design-patterns/src/estruturais/decorators/IcmsComIss.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Alura\DesignPattern\estruturais\decorators;
+
+use Alura\DesignPattern\comportamentais\strategy\Impostos\Icms;
+use Alura\DesignPattern\comportamentais\strategy\Impostos\Imposto;
+use Alura\DesignPattern\comportamentais\strategy\Impostos\Iss;
+use Alura\DesignPattern\Orcamento;
+
+class IcmsComIss implements Imposto
+{
+	public function calculaImposto(Orcamento $orcamento): float
+	{
+		// Example implementation: sum ICMS (0.1) and ISS (0.06)
+		return (new Icms())->calculaImposto($orcamento) + (new Iss())->calculaImposto($orcamento);
+	}
+}
